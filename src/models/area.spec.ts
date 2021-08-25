ed6fb82784496785d657be0b00da784f3763acab
Alex Shakirov <a.shakirov@infinnity.ru>
2021-08-25 16:10:34 +0500

    added funcs

--- src/models/area.spec.ts
+++ src/models/area.spec.ts
@@ -1,8 +1,9 @@
 import { Area } from './area';
 import { Snake } from './snake';
 import { CellDirection } from './cell-direction';
+import { GameOver } from '../errors/game-over';
 
-describe('Type', () => {
+describe('Area', () => {
     describe('constructor', () => {
         test('default', () => {
             const area = new Area(23, 35);
@@ -22,13 +23,219 @@ describe('Type', () => {
             const snake = new Snake([CellDirection.Right, CellDirection.Up]);
             const position: [number, number] = [12, 24];
 
-            const area = new Area(23, 35, snake, position);
+            const area = new Area(23, 35, { snake, snakePosition: position });
 
             expect(area.height).toBe(23);
             expect(area.width).toBe(35);
 
             expect(area.snake).toBe(snake);
             expect(area.snakePosition).toBe(position);
+        });
+    });
+
+    describe('getSnakeCells', () => {
+        test('for straight snake', () => {
+            const snake = new Snake([CellDirection.Up, CellDirection.Up, CellDirection.Up, CellDirection.Up]);
+            const area = new Area(20, 20, { snake, snakePosition: [0, 0] });
+
+            const result = area.getSnakeCells();
+            const expected = [[0, 0], [0, 1], [0, 2], [0, 3]];
+
+            expect(result).toStrictEqual(expected);
+        });
+
+        test('for curve snake', () => {
+            const snake = new Snake([CellDirection.Up, CellDirection.Up, CellDirection.Up, CellDirection.Left, CellDirection.Left, CellDirection.Down, CellDirection.Right]);
+            const area = new Area(20, 20, { snake, snakePosition: [0, 0] });
+
+            const result = area.getSnakeCells();
+            const expected = [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [1, 1]];
+
+            expect(result).toStrictEqual(expected);
+        });
+    });
+
+    describe('getNextSnakePosition', () => {
+        test('for up head', () => {
+            const snake = new Snake([CellDirection.Up]);
+            const area = new Area(9, 9, { snake, snakePosition: [5, 5] });
+
+            const result = area.getNextSnakePosition();
+            const expected = [5, 4];
+
+            expect(result).toStrictEqual(expected);
+        });
+
+        test('for right head', () => {
+            const snake = new Snake([CellDirection.Right]);
+            const area = new Area(9, 9, { snake, snakePosition: [5, 5] });
+
+            const result = area.getNextSnakePosition();
+            const expected = [6, 5];
+
+            expect(result).toStrictEqual(expected);
+        });
+
+        test('for down head', () => {
+            const snake = new Snake([CellDirection.Down]);
+            const area = new Area(9, 9, { snake, snakePosition: [5, 5] });
+
+            const result = area.getNextSnakePosition();
+            const expected = [5, 6];
+
+            expect(result).toStrictEqual(expected);
+        });
+
+        test('for left head', () => {
+            const snake = new Snake([CellDirection.Left]);
+            const area = new Area(9, 9, { snake, snakePosition: [5, 5] });
+
+            const result = area.getNextSnakePosition();
+            const expected = [4, 5];
+
+            expect(result).toStrictEqual(expected);
+        });
+    });
+
+    describe('checkBounds', () => {
+        describe('check vertical bounds', () => {
+            test('y bottom overflow', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [3, -1];
+
+                const result = area.checkBounds(cell);
+                const expected = false;
+
+                expect(result).toBe(expected);
+            });
+
+            test('y top overflow', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [3, 5];
+
+                const result = area.checkBounds(cell);
+                const expected = false;
+
+                expect(result).toBe(expected);
+            });
+        });
+
+        describe('check horizontal bounds', () => {
+            test('x bottom overflow', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [-1, 3];
+
+                const result = area.checkBounds(cell);
+                const expected = false;
+
+                expect(result).toBe(expected);
+            });
+
+            test('x top overflow', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [5, 3];
+
+                const result = area.checkBounds(cell);
+                const expected = false;
+
+                expect(result).toBe(expected);
+            });
+        });
+
+        describe('check bounds circumstances', () => {
+            test('x 0 value should be ok', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [0, 3];
+
+                const result = area.checkBounds(cell);
+                const expected = true;
+
+                expect(result).toBe(expected);
+            });
+
+            test('x top border should be ok', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [4, 3];
+
+                const result = area.checkBounds(cell);
+                const expected = true;
+
+                expect(result).toBe(expected);
+            });
+
+            test('y 0 value should be ok', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [3, 0];
+
+                const result = area.checkBounds(cell);
+                const expected = true;
+
+                expect(result).toBe(expected);
+            });
+
+            test('y top border should be ok', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [3, 4];
+
+                const result = area.checkBounds(cell);
+                const expected = true;
+
+                expect(result).toBe(expected);
+            });
+
+            test('center should be ok', () => {
+                const area = new Area(5, 5);
+                const cell: [number, number] = [2, 2];
+
+                const result = area.checkBounds(cell);
+                const expected = true;
+
+                expect(result).toBe(expected);
+            });
+        });
+    });
+
+    describe('isSnakeCell', () => {
+        let area: Area = null;
+
+        beforeEach(() => {
+            const snake = new Snake([CellDirection.Up, CellDirection.Up, CellDirection.Up, CellDirection.Left, CellDirection.Left, CellDirection.Down, CellDirection.Right]);
+
+            area = new Area(20, 20, { snake, snakePosition: [0, 0] });
+
+            // [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [1, 1]];
+        })
+
+        test('check free cell', () => {
+            const result = area.isSnakeCell([1, 0]);
+            const expected = false;
+
+            expect(result).toBe(expected);
+        })
+
+        test('check snake cell', () => {
+            const result = area.isSnakeCell([1, 1]);
+            const expected = true;
+
+            expect(result).toBe(expected);
+        })
+    });
+
+    describe('tick', () => {
+        describe('Game over', () => {
+            test('break beyond', () => {
+                const snake = new Snake([CellDirection.Up,]);
+                const area = new Area(20, 20, { snake, snakePosition: [0, 0] });
+
+                expect(() => area.tick()).toThrowError(new GameOver('You break area beyond'));
+            });
+
+            test('break beyond', () => {
+                const snake = new Snake([CellDirection.Down, CellDirection.Right, CellDirection.Up, CellDirection.Left, CellDirection.Left]);
+                const area = new Area(20, 20, { snake, snakePosition: [1, 0] });
+
+                expect(() => area.tick()).toThrowError(new GameOver('You did eat yourself'));
+            });
         });
     });
 });
--- src/models/area.ts
+++ src/models/area.ts
@@ -1,6 +1,15 @@
 import { Snake } from './snake';
+import { CellDirection } from './cell-direction';
+import _ from 'declarray';
+import { GameOver } from '../errors/game-over';
 
 declare type Position = [number, number];
+
+interface AreaArgs {
+    snake?: Snake;
+    snakePosition?: Position;
+    foodCells?: Position[];
+}
 
 export class Area {
     public readonly height: number;
@@ -8,19 +17,91 @@ export class Area {
 
     public readonly snake: Snake;
 
+    public readonly snakeCells: Position[];
+    public readonly foodCells: Position[];
+
     public readonly snakePosition: Position;
 
-    public constructor(height: number, width: number);
-    public constructor(height: number, width: number, snake: Snake, position: Position);
-    public constructor(height: number, width: number, snake?: Snake, position?: Position) {
-        this.snake = snake ?? new Snake();
+    private get snakeHead(): CellDirection {
+        return this.snake.head;
+    }
 
+    public constructor(height: number, width: number);
+    public constructor(height: number, width: number, args: AreaArgs);
+    public constructor(height: number, width: number, args?: AreaArgs) {
         this.height = height;
         this.width = width;
 
-        this.snakePosition = position ?? [
+        this.snake = args?.snake ?? new Snake();
+        this.snakePosition = args?.snakePosition ?? [
             (height / 2) | 0,
             (width / 2) | 0,
         ];
+
+        this.snakeCells = this.getSnakeCells();
+
+        this.foodCells = args?.foodCells ?? [];
+    }
+
+    public tick(): Area {
+        const nextCell = this.getNextSnakePosition();
+
+        if (!this.checkBounds(nextCell)) {
+            throw new GameOver('You break area beyond');
+        }
+
+        if (this.isSnakeCell(nextCell)) {
+            throw new GameOver('You did eat yourself');
+        }
+
+        return undefined;
+    }
+
+    public isSnakeCell([x, y]: Position): boolean {
+        return  _(this.snakeCells).any(([s_X, s_Y]) => s_X === x && s_Y === y);
+    }
+
+    public checkBounds(position: Position): boolean {
+        const [x, y] = position;
+        if (x < 0 || x >= this.width) {
+            return false;
+        } else if (y < 0 || y >= this.height) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public getNextSnakePosition(): Position {
+        const coord = this.snakePosition;
+
+        switch (this.snakeHead) {
+            case CellDirection.Up: return [coord[0], coord[1] - 1];
+            case CellDirection.Right: return [coord[0] + 1, coord[1]];
+            case CellDirection.Down: return [coord[0], coord[1] + 1];
+            case CellDirection.Left: return [coord[0] - 1, coord[1]];
+        }
+    }
+
+    public getSnakeCells(): Position[] {
+        const coords = [this.snakePosition];
+
+        let last = this.snakePosition;
+
+        for (let i = 1; i < this.snake.body.length; i++) {
+            last = this.getNextSectionCoord(last, this.snake.body[i]);
+            coords.push(last);
+        }
+
+        return coords;
+    }
+
+    private getNextSectionCoord(coord: Position, section: CellDirection): Position {
+        switch (section) {
+            case CellDirection.Up: return [coord[0], coord[1] + 1];
+            case CellDirection.Right: return [coord[0] - 1, coord[1]];
+            case CellDirection.Down: return [coord[0], coord[1] - 1];
+            case CellDirection.Left: return [coord[0] + 1, coord[1]];
+        }
     }
 }
